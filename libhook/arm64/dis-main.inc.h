44bf87905dee442e037f5c9cae1930840e189d2d
史贵岭 <SHIGUILING063@pingan.com.cn>
2018-07-19 16:16:28 +0800

    Fixed crashes when the same function is hooked twice; sext was evalua…

--- libhook/arm64/dis-main.inc.h
+++ libhook/arm64/dis-main.inc.h
@@ -1,14 +1,13 @@
 static INLINE void P(adrlabel_label_unk_Xd_1_ADR)(tdis_ctx ctx, struct bitslice Xd, struct bitslice label) {
-    return P(pcrel)(ctx, ctx->base.pc + sext(bs_get(label, ctx->base.op), 22),
+    return P(pcrel)(ctx, ctx->base.pc + sext(bs_get(label, ctx->base.op), 21),
                     (struct arch_pcrel_info) {bs_get(Xd, ctx->base.op), PLM_ADR});
 }
 static INLINE void P(adrplabel_label_unk_Xd_1_ADRP)(tdis_ctx ctx, struct bitslice Xd, struct bitslice label) {
     return P(pcrel)(ctx,
                     (ctx->base.pc & ~0xfff) +
-                    (sext(bs_get(label, ctx->base.op), 22) << 12),
+                    (sext(bs_get(label, ctx->base.op), 21) << 12),
                     (struct arch_pcrel_info) {bs_get(Xd, ctx->base.op), PLM_ADR});
 }
-
 static INLINE void P(am_b_target_addr_B_1_B)(tdis_ctx ctx, struct bitslice addr) {
     return P(branch)(ctx, ctx->base.pc + sext(bs_get(addr, ctx->base.op), 26) * 4,
                      /*cc*/ 0);
